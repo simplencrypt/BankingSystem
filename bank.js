4738d97def6c198a26092b56b6d6cf2717fe0cc5
Pixar <166309218+simplencrypt@users.noreply.github.com>
2026-02-07 23:13:49 +0530

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,40 @@
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const feedback = document.getElementById('feedback');
+
+    fetch("https://script.google.com/macros/s/AKfycbz7U_iR3rfV1MYmpyQFQhVI0urefi5pL3ymF-UhbprFtdSNtEtuBqzIQlPrWyUuHZqg3w/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            feedback.innerText = "Login successful. Welcome.";
+            feedback.style.color = "green";
+
+            // Store user session
+            localStorage.setItem("loggedInUser", username);
+
+            // Temporary redirect
+            // window.location.href = "dashboard.html";
+        } else {
+            feedback.innerText = "Invalid credentials.";
+            feedback.style.color = "red";
+        }
+    })
+    .catch(error => {
+    feedback.innerText = "ERROR: " + error;
+    feedback.style.color = "red";
+    console.error("Fetch error:", error);
+});
+
+});
